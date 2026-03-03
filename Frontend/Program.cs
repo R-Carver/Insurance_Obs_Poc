040605db629ed17088fefa25b3f46a9c0a01a0e6
R-Carver <davidmakowski00@gmail.com>
2026-03-03 13:19:42 +0100

    emit OpenTelemetry from Frontend

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -1,4 +1,6 @@
 using Frontend.Components;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,21 @@ builder.Services.AddHttpClient("precheck", c =>
 
 builder.Services.AddHttpClient("policy", c =>
     c.BaseAddress = new Uri(builder.Configuration["Services:PolicyBaseUrl"]!));
+
+// OTel
+builder.Services.AddOpenTelemetry()
+    .WithTracing(t =>
+    {
+        t.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("frontend"));
+        t.SetSampler(new AlwaysOnSampler()); // important for debugging
+        t.AddAspNetCoreInstrumentation();
+        t.AddHttpClientInstrumentation();
+        t.AddOtlpExporter(o =>
+        {
+            o.Endpoint = new Uri("http://otel-collector.insurance.svc.cluster.local:4317");
+            o.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+        });
+    });
 
 var app = builder.Build();
 
